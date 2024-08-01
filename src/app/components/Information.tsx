09bbe7ce382e993ce28bc0c05b14765550c314a1
Monica Min <minm@usc.edu>
2024-08-01 16:31:07 -0700

    styled information for About page

--- src/app/components/Information.tsx
+++ src/app/components/Information.tsx
@@ -9,9 +9,9 @@ export default function Information() {
     <div className={"infoBody"}>
         <h2>Information</h2>
         <div>
-        <h2>Capabilities of the Linter</h2>
+        <h3>Capabilities of the Linter</h3>
         <ul>
-            <li>Components with the keyword @PLAYBOOK_HELPER_SKIP in their description will automatically be skipped over. This keyword has automatically been added to all country assets, team logos, and company logos that will have loose hex values. </li>
+            <li>Components with the keyword <span className='code'>@PLAYBOOK_HELPER_PLUGIN: SKIP</span> in their description will automatically be skipped over. This keyword has automatically been added to all country assets, team logos, and company logos that will have loose hex values. </li>
             <li>You must have a library imported in order for your variable/style to count as “Imported”. Press Assets and import your desired library.</li>
             <li>Any layers that are locked will automatically be skipped and not linted.</li>
             <li>Elements with a spacing or corner radius value of 0 will not be linted/flagged.</li>
@@ -19,7 +19,7 @@ export default function Information() {
         </div>
 
         <div>
-            <h2>Capabilites of the Content Reel</h2>
+            <h3>Capabilites of the Content Reel</h3>
             <ul>
                 <li>The content reel only scans for text layers and layers with an image fill.</li>
                 <li>The content reel groups text/image layers that are within a frame together so all information can be related to one another.</li>
